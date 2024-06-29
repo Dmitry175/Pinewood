aab285258f30ea96f4bf7d46489140c5801cc72b
Dmitry <Dmitry@DESKTOP-P0N374A>
2024-06-29 12:18:49 +0100

    Add project files.

---
+++ Database/DatabaseInitializer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Pinewood.Database
+{
+    public class DatabaseInitializer
+    {
+        public static void Initialize(string connectionString)
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var createTableCommand = @"
+                    CREATE TABLE IF NOT EXISTS Products (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Name TEXT NOT NULL,
+                        Email TEXT NOT NULL
+                        Phone TEXT NOT NULL
+                    );";
+
+                using (SqliteCommand command = new SqliteCommand(createTableCommand, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                var insertDataCommand = @"
+                    INSERT OR IGNORE INTO Customers (Name, Email, Phone) VALUES ('John Doe', jd@email.com, 123456789);";
+
+                using (var command = new SqliteCommand(insertDataCommand, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using Pinewood.Components;
+using Pinewood.Database;
+
+namespace Pinewood
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            DatabaseInitializer.Initialize(connectionString);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
